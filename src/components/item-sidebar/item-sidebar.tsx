0f5269c9b684b05cac4750faa20b0c3de2df2081
XPA <xpadev@gmail.com>
2023-10-15 00:49:26 +0900

    feat: item-sidebar

--- src/components/item-sidebar/item-sidebar.tsx
+++ src/components/item-sidebar/item-sidebar.tsx
@@ -1,9 +1,15 @@
 import { api } from "~/utils/api";
 import { Sidebar } from "~/components/sidebar";
 import { SidebarItem } from "~/components/sidebar/sidebar-item";
+import { useRef, useState } from "react";
 
 const ItemSidebar = () => {
-  const { data: items } = api.items.getAll.useQuery();
+  const { data: items, refetch } = api.items.getAll.useQuery(undefined, {
+    cacheTime: 0,
+  });
+  const [val, setVal] = useState("");
+  const ref = useRef<HTMLDialogElement>(null);
+  const mutation = api.items.createItem.useMutation();
   const parts =
     items?.map((task) => {
       return {
@@ -11,15 +17,51 @@ const ItemSidebar = () => {
         href: `/items/${task.id}`,
       };
     }) ?? [];
+  const append = () => {
+    ref.current?.showModal();
+  };
+  const create = () => {
+    setVal("");
+    void (async () => {
+      mutation.mutate({ name: val });
+      await refetch();
+    })();
+  };
   return (
-    <Sidebar className={"fixed left-0 top-0 bg-base-300"}>
-      <li className="menu-title">Items</li>
-      {parts.map((item) => {
-        return (
-          <SidebarItem name={item.name} href={item.href} key={item.href} />
-        );
-      })}
-    </Sidebar>
+    <>
+      <Sidebar className={"fixed left-0 top-0 bg-base-300"}>
+        <li onClick={append}>
+          <span>Add Item</span>
+        </li>
+        <li className="menu-title">Items</li>
+        {parts.map((item) => {
+          return (
+            <SidebarItem name={item.name} href={item.href} key={item.href} />
+          );
+        })}
+      </Sidebar>
+      <dialog className="modal" ref={ref}>
+        <div className="modal-box">
+          <input
+            type="text"
+            placeholder="Enter Item Name"
+            className="input input-bordered w-full max-w-xs"
+            value={val}
+            onChange={(e) => setVal(e.target.value)}
+          />
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn" onClick={create}>
+                Create
+              </button>
+              <button className="btn" onClick={() => setVal("")}>
+                Close
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
   );
 };
 
--- src/server/api/routers/items.ts
+++ src/server/api/routers/items.ts
@@ -1,7 +1,17 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import z from "zod";
 
 export const itemsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.item.findMany();
   }),
+  createItem: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.db.item.create({
+        data: {
+          name: input.name,
+        },
+      });
+    }),
 });
