be05d673602996facec8e452cffedd7f5710073b
xpadev <xpadev@gmail.com>
2023-10-15 10:59:34 +0900

    improve: change to use million

--- src/pages/items/[itemId]/tasks/[taskId]/index.tsx
+++ src/pages/items/[itemId]/tasks/[taskId]/index.tsx
@@ -6,6 +6,8 @@ import { ItemSidebar } from "~/components/item-sidebar/item-sidebar";
 import { TaskSidebar } from "~/components/task-sidebar";
 import { TaskDetail } from "~/components/task-detail";
 
+import { block } from "million/react-server";
+
 const querySchema = z.object({
   itemId: z.preprocess((v) => Number(v), z.number()),
   taskId: z.preprocess((v) => Number(v), z.number()),
@@ -28,29 +30,40 @@ const TaskPage = () => {
       <TaskSidebar itemId={query.itemId} />
       <div className="ml-[40rem] h-screen flex flex-col">
         {task && <TaskDetail task={task} />}
-        <pre
-          className={
-            "whitespace-pre-wrap break-words max-w-full overflow-x-auto flex-grow bg-base-300"
-          }
-        >
-          <code>
-            {task?.logs.map((log) => {
-              return (
-                <p
-                  key={log.id}
-                  className={`${
-                    log.type === "err" && "bg-error text-error-content"
-                  } px-4`}
-                >
-                  {log.message}
-                </p>
-              );
-            })}
-          </code>
-        </pre>
+        {task && <LogViewerBlock task={task} />}
       </div>
     </div>
   );
 };
+
+const LogViewer = ({
+  task,
+}: {
+  task: { logs: { id: number; type: string; message: string }[] };
+}) => {
+  return (
+    <pre
+      className={
+        "whitespace-pre-wrap break-words max-w-full overflow-x-auto flex-grow bg-base-300"
+      }
+    >
+      <code>
+        {task?.logs.map((log) => {
+          return (
+            <p
+              key={log.id}
+              className={`${
+                log.type === "err" && "bg-error text-error-content"
+              } px-4`}
+            >
+              {log.message.replace(/.+\r/, "")}
+            </p>
+          );
+        })}
+      </code>
+    </pre>
+  );
+};
+const LogViewerBlock = block(LogViewer);
 
 export default TaskPage;
