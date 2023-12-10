b7cbc07501f8e3324c321c53ff917f9ac861be84
xpadev <xpadev@gmail.com>
2023-12-10 14:44:43 +0900

    fix: change toolchain and rebuild proto files

--- src/proto/main_grpc_pb.d.ts
+++ src/proto/main_grpc_pb.d.ts
@@ -1,76 +1,34 @@
+// GENERATED CODE -- DO NOT EDIT!
+
 // package: 
 // file: proto/main.proto
 
-/* tslint:disable */
-/* eslint-disable */
-
-import * as grpc from "grpc";
 import * as proto_main_pb from "../proto/main_pb";
-import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
+import * as grpc from "@grpc/grpc-js";
 
 interface ILoggingServiceService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
-    createTask: ILoggingServiceService_ICreateTask;
-    appendLog: ILoggingServiceService_IAppendLog;
-    endTask: ILoggingServiceService_IEndTask;
-}
-
-interface ILoggingServiceService_ICreateTask extends grpc.MethodDefinition<proto_main_pb.CreateTaskRequest, proto_main_pb.CreateTaskResponse> {
-    path: "/LoggingService/CreateTask";
-    requestStream: false;
-    responseStream: false;
-    requestSerialize: grpc.serialize<proto_main_pb.CreateTaskRequest>;
-    requestDeserialize: grpc.deserialize<proto_main_pb.CreateTaskRequest>;
-    responseSerialize: grpc.serialize<proto_main_pb.CreateTaskResponse>;
-    responseDeserialize: grpc.deserialize<proto_main_pb.CreateTaskResponse>;
-}
-interface ILoggingServiceService_IAppendLog extends grpc.MethodDefinition<proto_main_pb.AppendLogRequest, proto_main_pb.AppendLogResponse> {
-    path: "/LoggingService/AppendLog";
-    requestStream: false;
-    responseStream: false;
-    requestSerialize: grpc.serialize<proto_main_pb.AppendLogRequest>;
-    requestDeserialize: grpc.deserialize<proto_main_pb.AppendLogRequest>;
-    responseSerialize: grpc.serialize<proto_main_pb.AppendLogResponse>;
-    responseDeserialize: grpc.deserialize<proto_main_pb.AppendLogResponse>;
-}
-interface ILoggingServiceService_IEndTask extends grpc.MethodDefinition<proto_main_pb.EndTaskRequest, proto_main_pb.EndTaskResponse> {
-    path: "/LoggingService/EndTask";
-    requestStream: false;
-    responseStream: false;
-    requestSerialize: grpc.serialize<proto_main_pb.EndTaskRequest>;
-    requestDeserialize: grpc.deserialize<proto_main_pb.EndTaskRequest>;
-    responseSerialize: grpc.serialize<proto_main_pb.EndTaskResponse>;
-    responseDeserialize: grpc.deserialize<proto_main_pb.EndTaskResponse>;
+  createTask: grpc.MethodDefinition<proto_main_pb.CreateTaskRequest, proto_main_pb.CreateTaskResponse>;
+  appendLog: grpc.MethodDefinition<proto_main_pb.AppendLogRequest, proto_main_pb.AppendLogResponse>;
+  endTask: grpc.MethodDefinition<proto_main_pb.EndTaskRequest, proto_main_pb.EndTaskResponse>;
 }
 
 export const LoggingServiceService: ILoggingServiceService;
 
-export interface ILoggingServiceServer {
-    createTask: grpc.handleUnaryCall<proto_main_pb.CreateTaskRequest, proto_main_pb.CreateTaskResponse>;
-    appendLog: grpc.handleUnaryCall<proto_main_pb.AppendLogRequest, proto_main_pb.AppendLogResponse>;
-    endTask: grpc.handleUnaryCall<proto_main_pb.EndTaskRequest, proto_main_pb.EndTaskResponse>;
+export interface ILoggingServiceServer extends grpc.UntypedServiceImplementation {
+  createTask: grpc.handleUnaryCall<proto_main_pb.CreateTaskRequest, proto_main_pb.CreateTaskResponse>;
+  appendLog: grpc.handleUnaryCall<proto_main_pb.AppendLogRequest, proto_main_pb.AppendLogResponse>;
+  endTask: grpc.handleUnaryCall<proto_main_pb.EndTaskRequest, proto_main_pb.EndTaskResponse>;
 }
 
-export interface ILoggingServiceClient {
-    createTask(request: proto_main_pb.CreateTaskRequest, callback: (error: grpc.ServiceError | null, response: proto_main_pb.CreateTaskResponse) => void): grpc.ClientUnaryCall;
-    createTask(request: proto_main_pb.CreateTaskRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_main_pb.CreateTaskResponse) => void): grpc.ClientUnaryCall;
-    createTask(request: proto_main_pb.CreateTaskRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_main_pb.CreateTaskResponse) => void): grpc.ClientUnaryCall;
-    appendLog(request: proto_main_pb.AppendLogRequest, callback: (error: grpc.ServiceError | null, response: proto_main_pb.AppendLogResponse) => void): grpc.ClientUnaryCall;
-    appendLog(request: proto_main_pb.AppendLogRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_main_pb.AppendLogResponse) => void): grpc.ClientUnaryCall;
-    appendLog(request: proto_main_pb.AppendLogRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_main_pb.AppendLogResponse) => void): grpc.ClientUnaryCall;
-    endTask(request: proto_main_pb.EndTaskRequest, callback: (error: grpc.ServiceError | null, response: proto_main_pb.EndTaskResponse) => void): grpc.ClientUnaryCall;
-    endTask(request: proto_main_pb.EndTaskRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_main_pb.EndTaskResponse) => void): grpc.ClientUnaryCall;
-    endTask(request: proto_main_pb.EndTaskRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_main_pb.EndTaskResponse) => void): grpc.ClientUnaryCall;
-}
-
-export class LoggingServiceClient extends grpc.Client implements ILoggingServiceClient {
-    constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
-    public createTask(request: proto_main_pb.CreateTaskRequest, callback: (error: grpc.ServiceError | null, response: proto_main_pb.CreateTaskResponse) => void): grpc.ClientUnaryCall;
-    public createTask(request: proto_main_pb.CreateTaskRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_main_pb.CreateTaskResponse) => void): grpc.ClientUnaryCall;
-    public createTask(request: proto_main_pb.CreateTaskRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_main_pb.CreateTaskResponse) => void): grpc.ClientUnaryCall;
-    public appendLog(request: proto_main_pb.AppendLogRequest, callback: (error: grpc.ServiceError | null, response: proto_main_pb.AppendLogResponse) => void): grpc.ClientUnaryCall;
-    public appendLog(request: proto_main_pb.AppendLogRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_main_pb.AppendLogResponse) => void): grpc.ClientUnaryCall;
-    public appendLog(request: proto_main_pb.AppendLogRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_main_pb.AppendLogResponse) => void): grpc.ClientUnaryCall;
-    public endTask(request: proto_main_pb.EndTaskRequest, callback: (error: grpc.ServiceError | null, response: proto_main_pb.EndTaskResponse) => void): grpc.ClientUnaryCall;
-    public endTask(request: proto_main_pb.EndTaskRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_main_pb.EndTaskResponse) => void): grpc.ClientUnaryCall;
-    public endTask(request: proto_main_pb.EndTaskRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_main_pb.EndTaskResponse) => void): grpc.ClientUnaryCall;
+export class LoggingServiceClient extends grpc.Client {
+  constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
+  createTask(argument: proto_main_pb.CreateTaskRequest, callback: grpc.requestCallback<proto_main_pb.CreateTaskResponse>): grpc.ClientUnaryCall;
+  createTask(argument: proto_main_pb.CreateTaskRequest, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<proto_main_pb.CreateTaskResponse>): grpc.ClientUnaryCall;
+  createTask(argument: proto_main_pb.CreateTaskRequest, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<proto_main_pb.CreateTaskResponse>): grpc.ClientUnaryCall;
+  appendLog(argument: proto_main_pb.AppendLogRequest, callback: grpc.requestCallback<proto_main_pb.AppendLogResponse>): grpc.ClientUnaryCall;
+  appendLog(argument: proto_main_pb.AppendLogRequest, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<proto_main_pb.AppendLogResponse>): grpc.ClientUnaryCall;
+  appendLog(argument: proto_main_pb.AppendLogRequest, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<proto_main_pb.AppendLogResponse>): grpc.ClientUnaryCall;
+  endTask(argument: proto_main_pb.EndTaskRequest, callback: grpc.requestCallback<proto_main_pb.EndTaskResponse>): grpc.ClientUnaryCall;
+  endTask(argument: proto_main_pb.EndTaskRequest, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<proto_main_pb.EndTaskResponse>): grpc.ClientUnaryCall;
+  endTask(argument: proto_main_pb.EndTaskRequest, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<proto_main_pb.EndTaskResponse>): grpc.ClientUnaryCall;
 }
--- src/proto/main_pb.d.ts
+++ src/proto/main_pb.d.ts
@@ -1,178 +1,181 @@
 // package: 
 // file: proto/main.proto
 
-/* tslint:disable */
-/* eslint-disable */
-
 import * as jspb from "google-protobuf";
 import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
 
-export class Log extends jspb.Message { 
-    getTaskid(): number;
-    setTaskid(value: number): Log;
-    getType(): Log.LogType;
-    setType(value: Log.LogType): Log;
-    getMessage(): string;
-    setMessage(value: string): Log;
+export class Log extends jspb.Message {
+  getTaskid(): number;
+  setTaskid(value: number): void;
 
-    hasAt(): boolean;
-    clearAt(): void;
-    getAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
-    setAt(value?: google_protobuf_timestamp_pb.Timestamp): Log;
+  getType(): Log.LogTypeMap[keyof Log.LogTypeMap];
+  setType(value: Log.LogTypeMap[keyof Log.LogTypeMap]): void;
 
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): Log.AsObject;
-    static toObject(includeInstance: boolean, msg: Log): Log.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: Log, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): Log;
-    static deserializeBinaryFromReader(message: Log, reader: jspb.BinaryReader): Log;
+  getMessage(): string;
+  setMessage(value: string): void;
+
+  hasAt(): boolean;
+  clearAt(): void;
+  getAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setAt(value?: google_protobuf_timestamp_pb.Timestamp): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Log.AsObject;
+  static toObject(includeInstance: boolean, msg: Log): Log.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Log, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Log;
+  static deserializeBinaryFromReader(message: Log, reader: jspb.BinaryReader): Log;
 }
 
 export namespace Log {
-    export type AsObject = {
-        taskid: number,
-        type: Log.LogType,
-        message: string,
-        at?: google_protobuf_timestamp_pb.Timestamp.AsObject,
-    }
+  export type AsObject = {
+    taskid: number,
+    type: Log.LogTypeMap[keyof Log.LogTypeMap],
+    message: string,
+    at?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+  }
 
-    export enum LogType {
-    STDOUT = 0,
-    STDERR = 1,
-    }
+  export interface LogTypeMap {
+    STDOUT: 0;
+    STDERR: 1;
+  }
 
+  export const LogType: LogTypeMap;
 }
 
-export class CreateTaskRequest extends jspb.Message { 
-    getItemid(): number;
-    setItemid(value: number): CreateTaskRequest;
-    getIssuer(): string;
-    setIssuer(value: string): CreateTaskRequest;
-    getProcessid(): number;
-    setProcessid(value: number): CreateTaskRequest;
-    getWorkdir(): string;
-    setWorkdir(value: string): CreateTaskRequest;
-    getCommand(): string;
-    setCommand(value: string): CreateTaskRequest;
+export class CreateTaskRequest extends jspb.Message {
+  getItemid(): number;
+  setItemid(value: number): void;
 
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): CreateTaskRequest.AsObject;
-    static toObject(includeInstance: boolean, msg: CreateTaskRequest): CreateTaskRequest.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: CreateTaskRequest, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): CreateTaskRequest;
-    static deserializeBinaryFromReader(message: CreateTaskRequest, reader: jspb.BinaryReader): CreateTaskRequest;
+  getIssuer(): string;
+  setIssuer(value: string): void;
+
+  getProcessid(): number;
+  setProcessid(value: number): void;
+
+  getWorkdir(): string;
+  setWorkdir(value: string): void;
+
+  getCommand(): string;
+  setCommand(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CreateTaskRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: CreateTaskRequest): CreateTaskRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: CreateTaskRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CreateTaskRequest;
+  static deserializeBinaryFromReader(message: CreateTaskRequest, reader: jspb.BinaryReader): CreateTaskRequest;
 }
 
 export namespace CreateTaskRequest {
-    export type AsObject = {
-        itemid: number,
-        issuer: string,
-        processid: number,
-        workdir: string,
-        command: string,
-    }
+  export type AsObject = {
+    itemid: number,
+    issuer: string,
+    processid: number,
+    workdir: string,
+    command: string,
+  }
 }
 
-export class CreateTaskResponse extends jspb.Message { 
-    getTaskid(): number;
-    setTaskid(value: number): CreateTaskResponse;
+export class CreateTaskResponse extends jspb.Message {
+  getTaskid(): number;
+  setTaskid(value: number): void;
 
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): CreateTaskResponse.AsObject;
-    static toObject(includeInstance: boolean, msg: CreateTaskResponse): CreateTaskResponse.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: CreateTaskResponse, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): CreateTaskResponse;
-    static deserializeBinaryFromReader(message: CreateTaskResponse, reader: jspb.BinaryReader): CreateTaskResponse;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CreateTaskResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: CreateTaskResponse): CreateTaskResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: CreateTaskResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CreateTaskResponse;
+  static deserializeBinaryFromReader(message: CreateTaskResponse, reader: jspb.BinaryReader): CreateTaskResponse;
 }
 
 export namespace CreateTaskResponse {
-    export type AsObject = {
-        taskid: number,
-    }
+  export type AsObject = {
+    taskid: number,
+  }
 }
 
-export class AppendLogRequest extends jspb.Message { 
+export class AppendLogRequest extends jspb.Message {
+  hasLog(): boolean;
+  clearLog(): void;
+  getLog(): Log | undefined;
+  setLog(value?: Log): void;
 
-    hasLog(): boolean;
-    clearLog(): void;
-    getLog(): Log | undefined;
-    setLog(value?: Log): AppendLogRequest;
-
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): AppendLogRequest.AsObject;
-    static toObject(includeInstance: boolean, msg: AppendLogRequest): AppendLogRequest.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: AppendLogRequest, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): AppendLogRequest;
-    static deserializeBinaryFromReader(message: AppendLogRequest, reader: jspb.BinaryReader): AppendLogRequest;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): AppendLogRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: AppendLogRequest): AppendLogRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: AppendLogRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): AppendLogRequest;
+  static deserializeBinaryFromReader(message: AppendLogRequest, reader: jspb.BinaryReader): AppendLogRequest;
 }
 
 export namespace AppendLogRequest {
-    export type AsObject = {
-        log?: Log.AsObject,
-    }
+  export type AsObject = {
+    log?: Log.AsObject,
+  }
 }
 
-export class AppendLogResponse extends jspb.Message { 
-
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): AppendLogResponse.AsObject;
-    static toObject(includeInstance: boolean, msg: AppendLogResponse): AppendLogResponse.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: AppendLogResponse, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): AppendLogResponse;
-    static deserializeBinaryFromReader(message: AppendLogResponse, reader: jspb.BinaryReader): AppendLogResponse;
+export class AppendLogResponse extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): AppendLogResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: AppendLogResponse): AppendLogResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: AppendLogResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): AppendLogResponse;
+  static deserializeBinaryFromReader(message: AppendLogResponse, reader: jspb.BinaryReader): AppendLogResponse;
 }
 
 export namespace AppendLogResponse {
-    export type AsObject = {
-    }
+  export type AsObject = {
+  }
 }
 
-export class EndTaskRequest extends jspb.Message { 
-    getTaskid(): number;
-    setTaskid(value: number): EndTaskRequest;
-    getExitcode(): number;
-    setExitcode(value: number): EndTaskRequest;
+export class EndTaskRequest extends jspb.Message {
+  getTaskid(): number;
+  setTaskid(value: number): void;
 
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): EndTaskRequest.AsObject;
-    static toObject(includeInstance: boolean, msg: EndTaskRequest): EndTaskRequest.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: EndTaskRequest, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): EndTaskRequest;
-    static deserializeBinaryFromReader(message: EndTaskRequest, reader: jspb.BinaryReader): EndTaskRequest;
+  getExitcode(): number;
+  setExitcode(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): EndTaskRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: EndTaskRequest): EndTaskRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: EndTaskRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): EndTaskRequest;
+  static deserializeBinaryFromReader(message: EndTaskRequest, reader: jspb.BinaryReader): EndTaskRequest;
 }
 
 export namespace EndTaskRequest {
-    export type AsObject = {
-        taskid: number,
-        exitcode: number,
-    }
+  export type AsObject = {
+    taskid: number,
+    exitcode: number,
+  }
 }
 
-export class EndTaskResponse extends jspb.Message { 
-
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): EndTaskResponse.AsObject;
-    static toObject(includeInstance: boolean, msg: EndTaskResponse): EndTaskResponse.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: EndTaskResponse, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): EndTaskResponse;
-    static deserializeBinaryFromReader(message: EndTaskResponse, reader: jspb.BinaryReader): EndTaskResponse;
+export class EndTaskResponse extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): EndTaskResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: EndTaskResponse): EndTaskResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: EndTaskResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): EndTaskResponse;
+  static deserializeBinaryFromReader(message: EndTaskResponse, reader: jspb.BinaryReader): EndTaskResponse;
 }
 
 export namespace EndTaskResponse {
-    export type AsObject = {
-    }
+  export type AsObject = {
+  }
 }
+
