21ea9fe9f9511dbdd51b8eaab36ae32e98e6561c
xpadev <xpadev@gmail.com>
2023-12-15 04:01:41 +0900

    feat: custom server

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,5 @@
-import { startServer } from "~/server/grpc";
-
 console.log("instrumentation.ts triggered");
 
 export const register = () => {
   console.log("register triggered");
-  startServer();
 };
