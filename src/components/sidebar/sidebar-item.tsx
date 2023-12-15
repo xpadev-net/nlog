42612ebdd9a59b87e641a44fdc1e79ef16ffed94
xpadev <xpadev@gmail.com>
2023-12-15 10:24:05 +0900

    feat: scroller

--- src/components/sidebar/sidebar-item.tsx
+++ src/components/sidebar/sidebar-item.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import type { ReactNode } from "react";
+import { type ReactNode, useEffect, useRef } from "react";
 
 type props = {
   name: ReactNode;
@@ -11,8 +11,13 @@ const SidebarItem = ({ name, href }: props) => {
   const router = useRouter();
   const isActive =
     router.asPath === href || router.asPath.startsWith(href + "/");
+  const ref = useRef<HTMLLIElement>(null);
+  useEffect(() => {
+    if (!isActive) return;
+    ref.current?.scrollIntoView({ behavior: "smooth", block: "nearest" });
+  }, [href]);
   return (
-    <li>
+    <li ref={ref}>
       <Link href={href} className={isActive ? "active" : ""}>
         {name}
       </Link>
