a97522df988bafba82245d10cca5e2695ac7d217
xpadev <xpadev@gmail.com>
2023-12-16 01:51:47 +0900

    feat: ping handler

--- src/server/grpc/handler/index.ts
+++ src/server/grpc/handler/index.ts
@@ -3,11 +3,13 @@ import type { ILoggingServiceServer } from "~/proto/main_grpc_pb";
 import { createTask } from "./createTask";
 import { appendLog } from "./appendLog";
 import { endTask } from "./endTask";
+import { ping } from "./ping";
 
 export const getServerImplement = (): ILoggingServiceServer => {
   return {
     createTask,
     appendLog,
     endTask,
+    ping,
   };
 };
---
+++ src/server/grpc/handler/ping.ts
@@ -0,0 +1,35 @@
+import type { ILoggingServiceServer } from "~/proto/main_grpc_pb";
+import type { sendUnaryData, ServerUnaryCall } from "@grpc/grpc-js";
+import { type PingRequest, PingResponse } from "~/proto/main_pb";
+import { z } from "zod";
+import { db } from "~/server/db";
+
+const validate = z.object({
+  taskId: z.number(),
+});
+
+const ping: ILoggingServiceServer["ping"] = (
+  call: ServerUnaryCall<PingRequest, PingResponse>,
+  callback: sendUnaryData<PingResponse>,
+) => {
+  void (async () => {
+    const params = {
+      taskId: call.request.getTaskid(),
+    };
+    const data = validate.safeParse(params);
+    if (!data.success) {
+      callback(data.error, null);
+      return;
+    }
+    const input = data.data;
+    await db.task.update({
+      where: {
+        id: input.taskId,
+      },
+      data: {},
+    });
+    const response = new PingResponse();
+    callback(null, response);
+  })();
+};
+export { ping };
